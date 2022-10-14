52b1c74421a7a836f5b638db96ec3bb56f8e3c38
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2022-10-14 16:15:32 +0700

    Menambahkan ESLint, menambahkan handler untuk menampilkan seluruh buku dan detail buku

---
+++ src/handler.js
@@ -0,0 +1,126 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+// menyimpan buku
+const saveBookHandler = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  let finished;
+  if (pageCount === readPage) {
+    finished = true;
+  } else {
+    finished = false;
+  }
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  books.push(newBook);
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+  const isNoName = books.filter((book) => book.name == null || book.name === '');
+  const isReadPageMoreThanPageCount = books.filter((book) => book.readPage > book.pageCount);
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.header('Access-Control-Allow-Origin', '*');
+    response.code(201);
+    return response;
+  }
+
+  if (isNoName) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (isReadPageMoreThanPageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'error',
+    message: 'Buku gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+// menampilkan seluruh buku
+const getAllBooksHandler = () => ({
+  status: 'success',
+  data: {
+    books,
+  },
+});
+
+// menampilkan detail buku
+const getBookByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const book = books.filter((b) => b.id === id)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+// mengubah data buku
+
+// menghapus buku
+
+module.exports = {
+  saveBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+};
